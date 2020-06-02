50d8cf3fd8fd1020af7ea9cf18d1f7df11d1bfa9
jingling <wangjingling@chanbuyer.com>
2020-06-02 14:51:35 +0800

    简单模式:第二题

---
+++ algorithm-learning-examples/src/main/java/com/admol/algorithm/leetcode/medium/Lc002.java
@@ -0,0 +1,22 @@
+package com.admol.algorithm.leetcode.medium;
+
+/**
+ * 给出两个非空 的链表用来表示两个非负的整数。
+ * 其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头
+ * @author : admol
+ * @Date : 2020/6/2
+ */
+public class Lc002{
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        return null;
+    }
+
+     public class ListNode {
+         int val;
+         ListNode next;
+         ListNode(int x) { val = x; }
+      }
+}
