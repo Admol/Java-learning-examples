b603cc62592dc39c99f8448a4d16df0e5cc2dc56
jingling <wangjingling@chanbuyer.com>
2020-06-11 18:03:04 +0800

    简单模式:第五十三题, 最大子序和

--- algorithm-learning-examples/src/main/java/com/admol/algorithm/leetcode/simple/Lc0053.java
+++ algorithm-learning-examples/src/main/java/com/admol/algorithm/leetcode/simple/Lc0053.java
@@ -1,5 +1,6 @@
 package com.admol.algorithm.leetcode.simple;
 
+
 /**
  * 最大子序和
  * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
@@ -30,10 +31,41 @@ public class Lc0053{
         return ans;
     }
 
+    /**
+     * 与原题相反, 求最小的和, 并返回子串的索引位置,用3个大小的数组拼接0,1存储子串的索引位置,2存储最小和
+     * @param nums [-2,5,-3,-1]
+     * @return [2,3,-4]
+     */
+    public static int[] minSubArray(int[] nums) {
+        int[] result = new int[3];
+        int ans = nums[0];
+        // 累加的和
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(sum < 0) {
+                // 前面的累加和小于0, 继续累加起来
+                sum += nums[i];
+                result[1] = i;
+            } else {
+                // 和大于等于0,放弃前面的累加
+                result[0] = i;
+                sum = nums[i];
+            }
+            // 选择是否要更新成计算的最大的和
+            ans = Math.min(ans, sum);
+        }
+        result[2] = ans;
+        return result;
+    }
+
     public static void main(String[] args){
         int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
-//        System.out.println(maxSubArray(nums));
+        System.out.println(maxSubArray(nums));
         nums = new int[]{-2,5,-3,-1};
         System.out.println(maxSubArray(nums));
+        int[] result = minSubArray(nums);
+        for(int i = 0; i < result.length; i++){
+            System.out.print(result[i] + " ");
+        }
     }
 }
