6b963d7cb7a64453afdfdcd03c28976c8461e1d0
jingling <wangjingling@chanbuyer.com>
2020-06-17 10:57:37 +0800

    简单模式: 第104题, 二叉树的深度

---
+++ algorithm-learning-examples/src/main/java/com/admol/algorithm/leetcode/TreeNode.java
@@ -0,0 +1,18 @@
+package com.admol.algorithm.leetcode;
+
+
+/**
+ * 二叉树
+ * @author : admol
+ * @Date : 2020/6/16
+ */
+public class TreeNode{
+    int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x,TreeNode l,TreeNode r) {
+        val = x;
+        left = l;
+        right = r;
+    }
+}
