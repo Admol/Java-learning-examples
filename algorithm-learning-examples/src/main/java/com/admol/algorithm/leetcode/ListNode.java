ee2ade1f32113d6ed0f4fa29fedc8dd1b1bf89b1
jingling <wangjingling@chanbuyer.com>
2020-06-24 11:34:04 +0800

    简单模式:第141题, 环形链表

---
+++ algorithm-learning-examples/src/main/java/com/admol/algorithm/leetcode/ListNode.java
@@ -0,0 +1,14 @@
+package com.admol.algorithm.leetcode;
+
+/**
+ * @author : admol
+ * @Date : 2020/6/24
+ */
+public class ListNode{
+    public ListNode next;
+    public int val;
+    ListNode(int x) {
+      val = x;
+      next = null;
+    }
+}
