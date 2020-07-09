d176c8a6b6b533d09215fab22ccfc783a12006f0
jingling <wangjingling@chanbuyer.com>
2020-07-09 11:29:00 +0800

    简单模式: 第342题, 4的幂

--- algorithm-learning-examples/src/main/java/com/admol/algorithm/leetcode/simple/Lc0326.java
+++ algorithm-learning-examples/src/main/java/com/admol/algorithm/leetcode/simple/Lc0326.java
@@ -24,7 +24,7 @@ public class Lc0326{
      * @param n
      * @return
      */
-    public boolean isPowerOfThree(int n) {
+    public static boolean isPowerOfThree(int n) {
         return n > 0 && 1162261467 % n == 0;
     }
 
