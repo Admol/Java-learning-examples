ed4e5f4e516f81adca7ffe715a0a86a3c7d95fcd
jingling <wangjingling@chanbuyer.com>
2020-07-28 15:51:39 +0800

    简单模式:链表: 剑指 Offer 18 . 删除链表的节点

---
+++ algorithm-learning-examples/src/main/java/com/admol/algorithm/leetcode/simple/list/Offer18.java
@@ -0,0 +1,54 @@
+package com.admol.algorithm.leetcode.simple.list;
+
+import com.admol.algorithm.leetcode.ListNode;
+
+/**
+ * 删除链表的节点
+ * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+ * 返回删除后的链表的头节点。
+ * 注意：此题对比原题有改动
+ *
+ * 示例 1:
+ * 输入: head = [4,5,1,9], val = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ * 示例 2:
+ * 输入: head = [4,5,1,9], val = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ *
+ * 说明：
+ * 题目保证链表中节点的值互不相同
+ * 若使用 C 或 C++ 语言，你不需要 free 或 delete 被删除的节点
+ *
+ * 链接：https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof
+ * @author : admol
+ * @Date : 2020/7/28
+ */
+public class Offer18{
+
+    public static ListNode deleteNode(ListNode head,int val) {
+        if(head == null){
+            return null;
+        }
+        // 哨兵
+        ListNode res = new ListNode(0);
+        res.next = head;
+
+        ListNode pre = res;
+        ListNode curr = pre.next;
+
+        while(curr != null){
+            if(curr.val == val){
+                pre.next = curr.next;
+            }
+            pre = curr;
+            curr = curr.next;
+        }
+        return res.next;
+    }
+
+    public static void main(String[] args){
+        System.out.println(deleteNode(new ListNode(1,new ListNode(2,new ListNode(3))),2));
+    }
+}
