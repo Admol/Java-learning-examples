ef62c9fee99190c4dda26a87d7ee7ec5886020c3
jingling <wangjingling@chanbuyer.com>
2020-07-28 16:29:17 +0800

    简单模式:链表: 剑指 Offer 52. 两个链表的第一个公共节点

---
+++ algorithm-learning-examples/src/main/java/com/admol/algorithm/leetcode/simple/list/Offer52.java
@@ -0,0 +1,85 @@
+package com.admol.algorithm.leetcode.simple.list;
+
+import com.admol.algorithm.leetcode.ListNode;
+
+/**
+ * 两个链表的第一个公共节点
+ * 输入两个链表，找出它们的第一个公共节点。
+ *
+ * 如下面的两个链表：
+ *
+ *
+ *
+ * 在节点 c1 开始相交。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+ * 输出：Reference of the node with value = 8
+ * 输入解释：相交节点的值为 8 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ *  
+ *
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+ * 输出：Reference of the node with value = 2
+ * 输入解释：相交节点的值为 2 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+ *  
+ *
+ * 示例 3：
+ *
+ *
+ *
+ * 输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+ * 输出：null
+ * 输入解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+ * 解释：这两个链表不相交，因此返回 null。
+ *  
+ *
+ * 注意：
+ * 如果两个链表没有交点，返回 null.
+ * 在返回结果后，两个链表仍须保持原有的结构。
+ * 可假定整个链表结构中没有循环。
+ * 程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+ * 本题与主站 160 题相同：https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+ *
+ * 链接：https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof
+ * @author : admol
+ * @Date : 2020/7/28
+ */
+public class Offer52{
+
+    /**
+     * A: 1 3 5 7 9
+     * B: 2 4 7 9
+     *
+     * new_A: 1 3 5 7 9 2 4 7 9
+     * new_B: 2 4 7 9 1 3 5 7 9
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public static ListNode getIntersectionNode(ListNode headA,ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        while(a != b){
+            a =  a == null ?  headB : a.next;
+            b =  b == null ?  headA : b.next;
+        }
+        return a;
+    }
+
+    public static void main(String[] args){
+        ListNode gg = new ListNode(7,new ListNode(9));
+        System.out.println(getIntersectionNode(
+                new ListNode(1,new ListNode(3,new ListNode(5,gg))),
+                new ListNode(2,new ListNode(4,gg))));
+    }
+
+}
