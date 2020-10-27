cac31fb0e2b2671e639b1b1b4d59d391f9c62b98
admol <wang@jingling.im>
2020-10-27 15:27:58 +0800

    JAVA 8日期api测试

--- java-base-learning-examples/src/main/java/net/admol/jingling/demo/time/JavaTimeTest.java
+++ java-base-learning-examples/src/main/java/net/admol/jingling/demo/time/JavaTimeTest.java
@@ -2,9 +2,7 @@ package net.admol.jingling.demo.time;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 import java.time.temporal.TemporalAdjusters;
 
 /**
@@ -23,13 +21,48 @@ public class JavaTimeTest {
         LocalTime nowTime =LocalTime.now();
         log.info("当前时间 nowTime:{}",nowTime);
         log.info("当前时间 nowTime:{}",nowTime.withNano(0));
-        LocalTime time = LocalTime.of(23,59,58);
-        log.info("指定时间 time:{}",time);
+        LocalTime localTime = LocalTime.of(23,59,58);
+        log.info("指定时间 time:{}",localTime);
         LocalTime time2 = LocalTime.parse("23:59:58");
         log.info("指定时间 time2:{}",time2);
         LocalDateTime now = LocalDateTime.now().withDayOfMonth(1);
         log.info("当前日期时间 now:{}",now);
 
+        final Clock clock = Clock.systemUTC();
+        System.out.println( clock.instant() );
+        System.out.println( clock.millis() );
+        System.out.println( clock.millis()  == System.currentTimeMillis());
 
+        // Get the local date and local time
+        final LocalDate date = LocalDate.now();
+        final LocalDate dateFromClock = LocalDate.now( clock );
+        System.out.println( date );
+        System.out.println( dateFromClock );
+        // Get the local date and local time
+        final LocalTime time = LocalTime.now();
+        final LocalTime timeFromClock = LocalTime.now( clock );
+        System.out.println( time );
+        System.out.println( timeFromClock );
+
+        // Get the local date/time
+        final LocalDateTime datetime = LocalDateTime.now();
+        final LocalDateTime datetimeFromClock = LocalDateTime.now( clock );
+        System.out.println( datetime );
+        System.out.println( datetimeFromClock );
+
+        // Get the zoned date/time
+        final ZonedDateTime zonedDatetime = ZonedDateTime.now();
+        final ZonedDateTime zonedDatetimeFromClock = ZonedDateTime.now( clock );
+        final ZonedDateTime zonedDatetimeFromZone = ZonedDateTime.now( ZoneId.of( "America/Los_Angeles" ) );
+        System.out.println( zonedDatetime );
+        System.out.println( zonedDatetimeFromClock );
+        System.out.println( zonedDatetimeFromZone );
+
+        // Get duration between two dates
+        final LocalDateTime from = LocalDateTime.of( 2014, Month.APRIL, 16, 0, 0, 0 );
+        final LocalDateTime to = LocalDateTime.of( 2015, Month.APRIL, 16, 23, 59, 59 );
+        final Duration duration = Duration.between( from, to );
+        System.out.println( "Duration in days: " + duration.toDays() );
+        System.out.println( "Duration in hours: " + duration.toHours() );
     }
 }
